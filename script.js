15d94d9ecae74b3b8e5722a46ad0533c4be97722
Héctor Espinoza <hespinoza@uchile.cl>
2020-03-13 14:16:53 -0300

    styles changes when pikachu appears, two points for scores deleted

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const body = document.querySelector('body');
 const charmander = document.querySelector('#charmander');
 const squirtle = document.querySelector('#squirtle')
 const bulbasaur = document.querySelector('#bulbasaur');
 const pikachu = document.querySelector('#pikachu');
 const optionsContainer = document.querySelector('.options-container');
-const userOptions = document.querySelector("option");
+const userOptions = document.querySelector('option');
+const resultsContainer = document.querySelector('.results-container')
 const result = document.querySelector('#result');
 const scores = document.querySelector('#scores');
 const userChoice = document.querySelector('#userChoice');
@@ -22,9 +24,12 @@ let computerScore = 0;
 /* pikachu is not available to use until the user gets 5 points */
 pikachu.style.display= 'none';
 
+/* Styles also change when pikachu is displayed */
 function showPikachu() {
     if (playerScore >= 5) {
         pikachu.style.display= 'block';
+        body.style.backgroundColor = '#d7acec';
+        resultsContainer.style.backgroundColor = '#6355b3';
     }
 }
 
@@ -43,19 +48,19 @@ function showUserOption(optionId){
     userOption.textContent = "You";
     if (optionId === 'charmander') {
         userImg.src = 'img/charmander.png';
-        userImg.width = "70";
+        userImg.width = "80";
 
     } else if (optionId === 'squirtle'){
         userImg.src = 'img/squirtle.png';
-        userImg.width = "70"; 
+        userImg.width = "80"; 
 
     } else if (optionId === 'pikachu'){
         userImg.src = 'img/pikachu.png';
-        userImg.width = "65"; 
+        userImg.width = "75"; 
 
     }else {
         userImg.src = 'img/bulbasaur.png';
-        userImg.width = "70"; 
+        userImg.width = "80"; 
     }
 }
 
@@ -63,19 +68,19 @@ function showComputerOption(computerSelection){
     computerOption.textContent = "Computer";
     if (computerSelection === 'fire') {
         computerImg.src = 'img/charmander.png';
-        computerImg.width = "70";
+        computerImg.width = "80";
 
     } else if (computerSelection === 'water'){
         computerImg.src = 'img/squirtle.png';
-        computerImg.width = "70"; 
+        computerImg.width = "80"; 
 
     } else if (computerSelection === 'electric'){
         computerImg.src = 'img/pikachu.png';
-        computerImg.width = "65"; 
+        computerImg.width = "75"; 
 
     } else {
         computerImg.src = 'img/bulbasaur.png';
-        computerImg.width = "70"; 
+        computerImg.width = "80"; 
     }
 
 }
@@ -91,7 +96,9 @@ function restartGame() {
     computerImg.src = '';
     optionsContainer.style.display= 'flex';
     choicesContainer.style.display= 'flex';
-    pikachu.style.display= 'none';  
+    pikachu.style.display= 'none';
+    body.style.backgroundColor = 'skyblue';
+    resultsContainer.style.backgroundColor = 'green';  
 }
 
 function scoreTracker() {
@@ -141,16 +148,16 @@ function playRound(playerSelection, computerSelection, optionId){
             showComputerOption(computerSelection);
 
         } else if (playerSelection === "electric" && computerSelection === "water" || playerSelection === "grass" && computerSelection === "electric") {
-            para.textContent = `You win! ${playerSelection} beats ${computerSelection} by 2 points!`;
-            playerScore += 2;
+            para.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerScore ++;
             para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
             showUserOption(optionId);
             showComputerOption(computerSelection);
             scoreTracker();
 
         } else if (playerSelection === "electric" && computerSelection === "grass" || playerSelection === "water" && computerSelection === "electric" ) {
-            para.textContent = `You lose! ${computerSelection} beats ${playerSelection} by 2 points`;
-            computerScore += 2;
+            para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+            computerScore ++;
             para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
             showUserOption(optionId);
             showComputerOption(computerSelection);
