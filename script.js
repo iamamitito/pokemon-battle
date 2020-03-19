39cced561fa94e04396919e28937378a6d193cd9
Héctor Espinoza <hespinoza@uchile.cl>
2020-03-19 02:44:27 -0300

    battle music added, pikachu sprite changed, display of battle changed

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ const scores = document.querySelector('#scores');
 const userChoice = document.querySelector('#userChoice');
 const computerChoice = document.querySelector('#computerChoice');
 const choicesContainer = document.querySelector('.choices');
+const audio = document.querySelector('.sound');
+const muteButton = document.querySelector('.mute-button');
 let userImg = document.createElement('img');
-let computerImg = document.createElement('img'); 
+let computerImg = document.createElement('img');
 let para = document.createElement('p');
 let para2 = document.createElement('p');
 let button = document.createElement('button');
@@ -22,12 +24,12 @@ let playerScore = 0;
 let computerScore = 0;
 
 /* pikachu is not available to use until the user gets 5 points */
-pikachu.style.display= 'none';
+pikachu.style.display = 'none';
 
-/* Styles also change when pikachu is displayed */
+/* Pikachu is displayed along with style changes */
 function showPikachu() {
     if (playerScore >= 5) {
-        pikachu.style.display= 'block';
+        pikachu.style.display = 'block';
         body.style.backgroundColor = '#d7acec';
         resultsContainer.style.backgroundColor = '#6355b3';
     }
@@ -44,43 +46,44 @@ function computerPlay() {
 }
 
 
-function showUserOption(optionId){
+function showUserOption(optionId) {
     userOption.textContent = "You";
     if (optionId === 'charmander') {
-        userImg.src = 'img/charmander.png';
-        userImg.width = "80";
+        userImg.src = 'img/usercharmander.png';
+        userImg.width = "70";
 
-    } else if (optionId === 'squirtle'){
-        userImg.src = 'img/squirtle.png';
-        userImg.width = "80"; 
+    } else if (optionId === 'squirtle') {
+        userImg.src = 'img/usersquirtle.png';
+        userImg.width = "70";
 
-    } else if (optionId === 'pikachu'){
-        userImg.src = 'img/pikachu.png';
-        userImg.width = "75"; 
 
-    }else {
-        userImg.src = 'img/bulbasaur.png';
-        userImg.width = "80"; 
+    } else if (optionId === 'pikachu') {
+        userImg.src = 'img/userpikachu.png';
+        userImg.width = "70";
+
+    } else {
+        userImg.src = 'img/userbulbasaur.png';
+        userImg.width = "70";
     }
 }
 
-function showComputerOption(computerSelection){
+function showComputerOption(computerSelection) {
     computerOption.textContent = "Computer";
     if (computerSelection === 'fire') {
         computerImg.src = 'img/charmander.png';
-        computerImg.width = "80";
+        computerImg.width = "70";
 
-    } else if (computerSelection === 'water'){
+    } else if (computerSelection === 'water') {
         computerImg.src = 'img/squirtle.png';
-        computerImg.width = "80"; 
+        computerImg.width = "70";
 
-    } else if (computerSelection === 'electric'){
+    } else if (computerSelection === 'electric') {
         computerImg.src = 'img/pikachu.png';
-        computerImg.width = "75"; 
+        computerImg.width = "70";
 
     } else {
         computerImg.src = 'img/bulbasaur.png';
-        computerImg.width = "80"; 
+        computerImg.width = "70";
     }
 
 }
@@ -94,11 +97,14 @@ function restartGame() {
     computerOption.textContent = "";
     userImg.src = '';
     computerImg.src = '';
-    optionsContainer.style.display= 'flex';
-    choicesContainer.style.display= 'flex';
-    pikachu.style.display= 'none';
+    optionsContainer.style.display = 'flex';
+    choicesContainer.style.display = 'flex';
+    pikachu.style.display = 'none';
     body.style.backgroundColor = 'skyblue';
-    resultsContainer.style.backgroundColor = 'green';  
+    resultsContainer.style.backgroundColor = 'green';
+    audio.pause();
+    audio.currentTime = 0;
+    audio.volume = 1;
 }
 
 function scoreTracker() {
@@ -106,93 +112,106 @@ function scoreTracker() {
         para.textContent = "Congrats! You're a great trainer!";
         button.textContent = "Restart game"
         result.appendChild(button);
-        optionsContainer.style.display= 'none';
-        choicesContainer.style.display= 'none';
-        button.addEventListener("click", function(){
+        optionsContainer.style.display = 'none';
+        choicesContainer.style.display = 'none';
+        button.addEventListener("click", function () {
             restartGame()
-          })
+        })
     } else if (computerScore >= 10) {
         para.textContent = "You're not such a good trainer, are you?";
         button.textContent = "Restart game"
         result.appendChild(button);
-        optionsContainer.style.display= 'none';
-        choicesContainer.style.display= 'none';
-        button.addEventListener("click", function(){
+        optionsContainer.style.display = 'none';
+        choicesContainer.style.display = 'none';
+        button.addEventListener("click", function () {
             restartGame()
-          })
+        })
     } else {
         return;
     }
 }
 
 
-function playRound(playerSelection, computerSelection, optionId){
-        if (computerSelection === playerSelection) {
-            para.textContent = `It's a tie, you both chose ${optionId}`;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
+function playRound(playerSelection, computerSelection, optionId) {
+    if (computerSelection === playerSelection) {
+        para.textContent = `It's a tie, you both chose ${optionId}`;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
 
-        } else if (playerSelection === "fire" && computerSelection === "grass" || playerSelection === "water" && computerSelection === "fire" || playerSelection === "grass" && computerSelection === "water") {
-            para.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-            playerScore++;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
-            scoreTracker();
+    } else if (playerSelection === "fire" && computerSelection === "grass" || playerSelection === "water" && computerSelection === "fire" || playerSelection === "grass" && computerSelection === "water") {
+        para.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
+        scoreTracker();
 
-        } else if (playerSelection === "electric" && computerSelection === "fire" || playerSelection === "fire" && computerSelection === "electric") {
-            para.textContent = `It's a tie, ${playerSelection} does NOT beat ${computerSelection}`;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
+    } else if (playerSelection === "electric" && computerSelection === "fire" || playerSelection === "fire" && computerSelection === "electric") {
+        para.textContent = `It's a tie, ${playerSelection} does NOT beat ${computerSelection}`;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
 
-        } else if (playerSelection === "electric" && computerSelection === "water" || playerSelection === "grass" && computerSelection === "electric") {
-            para.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-            playerScore ++;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
-            scoreTracker();
+    } else if (playerSelection === "electric" && computerSelection === "water" || playerSelection === "grass" && computerSelection === "electric") {
+        para.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
+        scoreTracker();
 
-        } else if (playerSelection === "electric" && computerSelection === "grass" || playerSelection === "water" && computerSelection === "electric" ) {
-            para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-            computerScore ++;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
-            scoreTracker();
+    } else if (playerSelection === "electric" && computerSelection === "grass" || playerSelection === "water" && computerSelection === "electric") {
+        para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
+        scoreTracker();
 
-        }else {
-            para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-            computerScore++;
-            para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
-            showUserOption(optionId);
-            showComputerOption(computerSelection);
-            scoreTracker();
-        }
+    } else {
+        para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        para2.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        showUserOption(optionId);
+        showComputerOption(computerSelection);
+        scoreTracker();
+    }
     showPikachu();
 }
 
+function startSound() {
+    if (!audio) return;
+    audio.play();
+}
 
-charmander.addEventListener('click', function(){
+function muteSound() {
+    audio.volume = 0;
+}
+
+
+charmander.addEventListener('click', function () {
     playRound("fire", computerPlay(), this.id)
-  });
-squirtle.addEventListener('click', function(){
+});
+squirtle.addEventListener('click', function () {
     playRound("water", computerPlay(), this.id)
-  });
-bulbasaur.addEventListener('click', function(){
+});
+bulbasaur.addEventListener('click', function () {
     playRound("grass", computerPlay(), this.id)
-  });
-pikachu.addEventListener('click', function(){
+});
+pikachu.addEventListener('click', function () {
     playRound("electric", computerPlay(), this.id)
 });
+charmander.addEventListener("click", startSound);
+squirtle.addEventListener("click", startSound);
+bulbasaur.addEventListener("click", startSound);
+muteButton.addEventListener("click", muteSound);
 
-  result.appendChild(para);
-  scores.appendChild(para2);
-  userChoice.appendChild(userOption);
-  userChoice.appendChild(userImg);
-  computerChoice.appendChild(computerOption);
-  computerChoice.appendChild(computerImg);
+result.appendChild(para);
+scores.appendChild(para2);
+userChoice.appendChild(userOption);
+userChoice.appendChild(userImg);
+computerChoice.appendChild(computerOption);
+computerChoice.appendChild(computerImg);
 
 
